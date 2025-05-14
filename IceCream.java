78ba61bda367c665e539c39766801bf2a5bfdfc7
J-uds <206530139+J-uds@users.noreply.github.com>
2025-05-14 18:56:27 +0200

    IceCream started

--- IceCream.java
+++ IceCream.java
@@ -1,13 +1,15 @@
+import java.util.Scanner;
+
 public class IceCream {
     public static void main(String[] args) {
-        
+
         //Vamos a hacer un programa que haga un pedido online por cada helado ordenado, vamos a escoger cuantas bolas de helado le pondremos y sus sabores.
 
         //Ejemplo de como debe mostrarse en la terminal:
-        /* 
+        /*
             ¿Cuántas bolas de helado quieres?: 3
 
-            ¡Perfecto! Ahora escoge de que sabor quieres cada helado: 
+            ¡Perfecto! Ahora escoge de que sabor quieres cada helado:
                 0. Chocolate
                 1. Fresa
                 2. Vainilla
@@ -16,7 +18,7 @@ public class IceCream {
                 0. Chocolate
                 1. Fresa
                 2. Vainilla
-            
+
             ¿Es correcto? presiona intro para continuar
 
             ¡Listo, tenemos tu orden! Acércate al mostrador y Robotina te entregará tu helado
@@ -29,6 +31,34 @@ public class IceCream {
         //Has de usar un array para almacenar los sabores de helado y un bucle para imprimirlos.
 
         //Mira en el ejemplo los saltos de línea que has de tener.
-    
+
+        String[] sabores = {"0. Chocolate", "1. Fresa", "2. Vainilla"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("¿Cuántas bolas de helado quieres?:");
+        int bolas = scanner.nextInt();
+        if (bolas > 3) {
+            System.out.println("Puedes elegir como máximo 3 bolas");
+            System.out.print("¿Cuántas bolas de helado quieres?:");
+            bolas = scanner.nextInt();
+        }
+        System.out.println("¡Perfecto! Ahora escoge el número del sabor que quieres cada helado: \n" +
+                "                0. Chocolate\n" +
+                "                1. Fresa\n" +
+                "                2. Vainilla");
+        int[] saborElegido = new int[bolas];
+        for (int i = 0; i < bolas; i++) {
+            saborElegido[i] = scanner.nextInt();
+        }
+        System.out.println("¡Gracias por tu orden! Has pedido los siguientes sabores:");
+        for (int i = 0; i < saborElegido.length; i++) {
+            int indice = saborElegido[i];
+            System.out.println(sabores[indice]);
+        }
+        System.out.println("¿Es correcto? Presiona intro para continuar");
+        scanner.nextLine();
+        System.out.println();
+        System.out.println("¡Listo, tenemos tu orden! Acércate al mostrador y Robotina te entregará tu helado");
+
+        scanner.close();
     }
 } 
