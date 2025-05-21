3869e62934ca2632ee77585a554e2618e6486d22
J-uds <206530139+J-uds@users.noreply.github.com>
2025-05-21 21:55:18 +0200

    RandomNumbers done

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -3,6 +3,9 @@ public class RandomNumbers {
         
         //Escribe un programa que genere un array de 2 dimensiones de 100 filas y 10 columnas  con números aleatorios del 0 al 99, a través de un bucle anidado (Si no te suenan estos conceptos, ¡búscalos!)
 
+        int[][] twoDArray = new int[100][10];
+        print2DArray(twoDArray);
+
     }
 
     /**
@@ -13,15 +16,27 @@ public class RandomNumbers {
      * Inside the function:
      * 1. generate random numbers from 0 to 99
      */
-
+        public static int randomNumbers() {
+            int randomNumber = (int)(Math.random()*100);
+            return randomNumber;
+        }
 
     /**
      * Function name: print2DArray
-     * 
+     *
      * @param integers (2D array int)
-     * 
+     *
      * Inside the function:
      * 1. nested loop that prints a 2D array using the randomNumber function
      */
+        public static void print2DArray (int[][] integers) {
+            for (int row = 0; row < integers.length; row++) {
+                for (int col = 0; col < integers[row].length; col++) {
+                    integers[row][col] = randomNumbers();
+                    System.out.print(integers[row][col] + "\t");
+                }
 
+                System.out.println();
+            }
+        }
 }
